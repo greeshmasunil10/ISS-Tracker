fd7d662471de8d982625ae015ebb528920207b51
unknown <greeshmasunil10@gmail.com>
2021-04-11 19:43:30 -0400

    first

---
+++ src/main/java/com/sunil/greeshma/ISSTracker/IssTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sunil.greeshma.ISSTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunil/greeshma/ISSTracker/IssTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunil.greeshma.ISSTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
